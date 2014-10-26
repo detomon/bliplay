b5fdc3d14b52b004004d45f4fc5e29d347786c8b
detomon <simon@monoxid.ch>
2014-10-26 11:36:46 +0100

    Bump version to 2.0

--- bliplay/bliplay.c
+++ bliplay/bliplay.c
@@ -35,6 +35,8 @@
 #include "BKWaveFileWriter.h"
 #include "BKString.h"
 
+#define BK_BLIPLAY_VERSION "2.0"
+
 #ifndef FD_COPY
 #define FD_COPY(src, dest) memcpy ((dest), (src), sizeof (*(dest)))
 #endif
