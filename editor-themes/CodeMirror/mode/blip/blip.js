e8a60a91e3b7f51da98658bcfa7cf06fbc62349c
detomon <simon@monoxid.ch>
2018-01-02 12:31:49 +0100

    Add Code Mirror syntax highlighting

---
+++ editor-themes/CodeMirror/mode/blip/blip.js
@@ -0,0 +1,119 @@
+(function(mod) {
+	if (typeof exports == "object" && typeof module == "object") // CommonJS
+		mod(require("../../lib/codemirror"));
+	else if (typeof define == "function" && define.amd) // AMD
+		define(["../../lib/codemirror"], mod);
+	else // Plain browser env
+		mod(CodeMirror);
+})(function(CodeMirror) {
+"use strict";
+
+CodeMirror.defineMode('blip', function() {
+
+	var words = {};
+	function define(style, string) {
+		var split = string.split(' ');
+		for(var i = 0; i < split.length; i++) {
+			words[split[i]] = style;
+		}
+	};
+
+	define('keyword', 'a adsr anv as at d data dc dcnv dn dr ds e g grp harm ' +
+		'i instr load m mt no noi noise p pal pk pnv pr ps pt pulsekernel pw r ' +
+		'rep rt s sam samp sample saw sawtooth si sin sinc sine sm sq sqr ' +
+		'square st stepticks stt sw t tickrate tr track tri triangle tstepticks ' +
+		'v vb vm vnv vs w wave x xb z');
+
+	// Notes
+  	var punc = "[]:;";
+	var notes =['a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'h'];
+
+	function tokenBase(stream, state) {
+		if (stream.eatSpace()) return null;
+
+		var ch = stream.next();
+
+		if (punc.indexOf(ch) > -1) {
+			return "punctuation";
+		}
+		if (ch === '\\') {
+			stream.next();
+			return null;
+		}
+		if (ch === '\'' || ch === '"') {
+			state.tokens.unshift(tokenString(ch, "string"));
+			return tokenize(stream, state);
+		}
+		if (ch === '%') {
+			stream.skipToEnd();
+			return 'comment';
+		}
+		if (notes.indexOf(ch) > -1) {
+			var next = stream.peek();
+
+			if (/[#\d]/.test(next)) {
+				if (notes.indexOf(ch + '#') > -1) {
+					stream.eat('#');
+				}
+				stream.eatWhile(/\d/);
+				return 'attribute';
+			}
+		}
+		if (/[-+]/.test(ch)) {
+			if (/\d/.test(stream.peek())) {
+				stream.eat(/[-+]/);
+				stream.eatWhile(/\d/);
+				return 'number';
+			}
+		}
+		if (/\d/.test(ch)) {
+			stream.eatWhile(/\d/);
+			if (stream.peek() === '/') {
+				stream.eat('/');
+				stream.eatWhile(/\d/);
+			}
+			return 'number';
+		}
+		stream.eatWhile(/[\w-]/);
+		var cur = stream.current();
+		return words.hasOwnProperty(cur) ? words[cur] : null;
+	}
+
+	function tokenString(quote, style) {
+		var close = quote;
+		return function(stream, state) {
+			var next, end = false, escaped = false;
+			while ((next = stream.next()) != null) {
+				if (next === close && !escaped) {
+					end = true;
+					break;
+				}
+				if (!escaped && next === quote && quote !== close) {
+					state.tokens.unshift(tokenString(quote, style))
+					return tokenize(stream, state)
+				}
+				escaped = !escaped && next === '\\';
+			}
+			if (end) state.tokens.shift();
+			return style;
+		};
+	};
+
+	function tokenize(stream, state) {
+		return (state.tokens[0] || tokenBase) (stream, state);
+	};
+
+	return {
+		startState: function() {return {tokens:[]};},
+		token: function(stream, state) {
+			return tokenize(stream, state);
+		},
+		closeBrackets: "[]''\"\"",
+		lineComment: '%',
+		fold: "brace"
+	};
+});
+
+CodeMirror.defineMIME('text/x-blip', 'blip');
+
+});
--- editor-themes/README.md
+++ editor-themes/README.md
@@ -3,34 +3,31 @@ Editor Themes
 
 This directory contains editor packages and themes that supports `blip` files.
 
-
 Installing for Sublime Text
 -----------------------------------
 
 To install the syntax highlighting package for Sublime Text,
-copy the directory `SublimeText/Blip` into the `Packages` folder, which can be
-opened in the menu with `Preferences > Browse Packages...`.
-
+copy the directory `SublimeText/Blip` into the `Packages` folder, which can be opened in the menu with `Preferences > Browse Packages...`.
 
 Installing for Notepad++
 -----------------------------------
 
-Copy `Notepad++/BlipNotepadPlusPlus.xml`
-and `Notepad++/Techmantium_Girly.xml`  into your Notepad++ themes directory.
+- Copy `Notepad++/BlipNotepadPlusPlus.xml` and `Notepad++/Techmantium_Girly.xml` into your Notepad++ themes directory, something like `C:\Program Files\Notepad++\themes\` or possibly `C:\Program Files (x86)\Notepad++\themes\`
+- If the themes directory doesn't exist, simply create one in your `Notepad++` directory
+- Launch Notepad++ and click `Settings` from the menu, then select `Style Configurator`
+- You'll want to select a dark theme for maximum effect. `Techmantium_Girly` is provided free from [http://timtrott.co.uk/notepad-colour-schemes](http://timtrott.co.uk/notepad-colour-schemes).
+- Close your theme window, and select menu item `Language > Define your language...` Click the `Import...` button
+- Select `BliplayNotepadPlusPlus.xml` from your `Notepad++\themes` directory
 
-Something like `C:\Program Files\Notepad++\themes\`
-or possibly `C:\Program Files (x86)\Notepad++\themes\`
+Using in CodeMirror
+-----------------------------------
 
-If the themes directory doesn't exist, simply create one in your `Notepad++` dir.
+Load the language script from `CodeMirror/mode/blip/blip.js` and use the mode `blip`:
 
-Launch Notepad++ and click `Settings` from the menu,
-then select `Style Configurator`.
-
-You'll want to select a dark theme for maximum effect.
-
-`Techmantium_Girly` is provided free from
-[http://timtrott.co.uk/notepad-colour-schemes](http://timtrott.co.uk/notepad-colour-schemes).
-
-Close your theme window, and select menu item `Language > Define your language...`   Click the `Import...` button.
-
-select `BliplayNotepadPlusPlus.xml` from your `Notepad++\themes` dir.
+```js
+var editor = CodeMirror.fromTextArea(document.getElementById('code'), {
+	mode: 'blip', // use mode 'blip'
+	lineNumbers: true,
+	matchBrackets: true,
+});
+```
