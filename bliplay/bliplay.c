0f6abbd9eb396abc3ff88ddbb79ce03eb723812a
detomon <simon@monoxid.ch>
2022-02-05 11:37:48 +0100

    Bump version to 3.2.2

--- bliplay/bliplay.c
+++ bliplay/bliplay.c
@@ -52,7 +52,7 @@
 #include "BKTK.h"
 #include "BlipKit.h"
 
-#define BK_BLIPLAY_VERSION "3.2.1"
+#define BK_BLIPLAY_VERSION "3.2.2"
 
 #ifndef FD_COPY
 #define FD_COPY(src, dest) memcpy ((dest), (src), sizeof (*(dest)))
